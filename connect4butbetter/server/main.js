1d012bf02e016662360320c8dde07872de704878
Hero Emenalom <heroemenalom73@gmail.com>
2024-12-30 22:41:37 -0500

    fix4

--- connect4butbetter/server/main.js
+++ connect4butbetter/server/main.js
@@ -13,7 +13,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
     cors: {
-        origin: ["http://localhost:3000",process.env.PROD_URL],
+        origin: ["http://localhost:3000",process.env.PROD_URL,"https://connect4butbetter.vercel.app"],
         methods: ['GET','POST']
     }
 });
